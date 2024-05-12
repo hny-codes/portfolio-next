4a1a0e2877dfec989d6cc0c11b036f6f703aba5d
Henry <hnguyhenry@protonmail.com>
2024-05-11 22:33:46 -0700

    Replace flex layout with grid layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import Skills from './components/Skills';
 export default function AboutPage() {
   return (
     <main className='text-white mb-6 sm:mb-0'>
-      <div className='px-8 sm:max-w-[var(--max-width)] sm:mx-auto sm:flex sm:gap-8 bg-gray-100/10 sm:rounded-3xl pt-8 pb-2'>
+      <div className='px-8 sm:max-w-[var(--max-width)] sm:mx-auto md:grid grid-cols-2 sm:gap-16 bg-gray-100/10 sm:rounded-3xl pt-8 pb-2'>
         <AboutInfo />
         <Skills />
       </div>
