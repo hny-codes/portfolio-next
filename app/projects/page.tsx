e6fd7079ed60065a733fe750cad28bce4c951428
Henry <hnguyhenry@protonmail.com>
2024-05-23 22:08:34 -0700

    Increase margin bottom of page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,7 +3,7 @@ import ProjectHeader from './components/ProjectHeader';
 
 export default function page() {
   return (
-    <main className='text-white mb-12 '>
+    <main className='text-white mb-16'>
       <div className='px-8 sm:max-w-[var(--max-width)] sm:mx-auto'>
         <section>
           <ProjectHeader />
