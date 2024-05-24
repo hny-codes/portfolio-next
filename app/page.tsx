11c7fdadd68a9f2bceb8b2178aea04d6f7e0248f
Henry <hnguyhenry@protonmail.com>
2024-05-24 00:53:04 -0700

    Dynamic import Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Hero from "./components/Hero"
+import dynamic from 'next/dynamic';
+
+const DynamicHero = dynamic(() => import('./components/Hero'));
 
 export default function Home() {
   return (
     <main className='sm:max-w-[var(--max-width)] sm:mx-auto'>
-      <Hero />
+      <DynamicHero />
     </main>
   );
 }
