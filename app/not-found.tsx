b91f0decbbe9af4c66dfe74b397d3db7e149f3ac
Henry <hnguyhenry@protonmail.com>
2024-05-23 22:37:25 -0700

    Add padding to button

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,10 +4,13 @@ import { Button } from '@/components/ui/button';
 export default function NotFound() {
   return (
     <div className='text-white p-8 text-center text-4xl'>
-      <h1>👀 Oops! - No adventure here, you&apos;ve hit a dead end!</h1>
+      <h1>👀 Oops! - No adventure here.. <br/> you&apos;ve hit a dead end!</h1>
       <Link href={'/'}>
-        <Button variant={'outline'} className='text-lg my-16 animate-bounce'>
-          Return Home
+        <Button
+          variant={'outline'}
+          className='text-lg my-16 py-8 animate-bounce'
+        >
+          Home
         </Button>
       </Link>
     </div>
